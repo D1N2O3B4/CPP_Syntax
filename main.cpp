933bd7b4b2b7cddce7cc7b1a11805a9b00aeb986
coLeman <99621038+D1N2O3B4@users.noreply.github.com>
2024-02-02 08:26:06 +0000

    Add files via upload
    Added Conditional Statements on A line example

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+#define iconst 30
+#define iname "Francis"
+
+int main() {
+//	int a = 001001;
+//	a<<=2;
+//	cout<<a<<endl; 
+//	cout<<iconst<<endl;
+//	cout<<iname<<endl;
+	int x;
+	x = true ? 23 : 56; 
+	cout<<x<<endl;
+	return 0;
+}
+
+
+
