1e81eb48f05c248b012f4fa4ea42a9decd01ba85
coLeman <99621038+D1N2O3B4@users.noreply.github.com>
2024-02-02 08:42:02 +0000

    Add files via upload
    Added another tenary conditonal statement

--- main.cpp
+++ main.cpp
@@ -1,18 +1,25 @@
 #include <iostream>
 using namespace std;
+//Another way to make constants
 #define iconst 30
 #define iname "Francis"
 
 int main() {
-//	int a = 001001;
-//	a<<=2;
-//	cout<<a<<endl; 
-//	cout<<iconst<<endl;
-//	cout<<iname<<endl;
+	int a = 001001;
+	//shift operator
+	a<<=2;
+	cout<<a<<endl; 
+	cout<<iconst<<endl;
+	cout<<iname<<endl;
+	//Conditonal on one line
 	int x;
 	x = true ? 23 : 56; 
 	cout<<x<<endl;
+	//Another Conditonal on one line
+	cout<< (iname == "Francis"? "Hello Francis" : "What is your name then")<<endl;
+	
 	return 0;
+	
 }
 
 
